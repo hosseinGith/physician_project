db44ccd6e69f1ee9dfa45b1a09a3cc24e2aadf56
hosseinGith <hosseinderees7@gmail.com>
2026-06-08 17:23:54 +0330

    add entity in blockedTime module

--- src/modules/users/doctor/blockedTimes/blockedTimes.module.ts
+++ src/modules/users/doctor/blockedTimes/blockedTimes.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { BlockedTimesController } from './blockedTimes.controller';
 import { BlockedTimesService } from './blockedTimes.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Users } from 'src/modules/users/users.entity';
-import { Doctors } from 'src/modules/users/doctor/doctors.entity';
+import { BlockedTimes } from './blockedTimes.entity';
 
 @Module({
- imports: [TypeOrmModule.forFeature([Users, Doctors]), HoursModule],
+ imports: [TypeOrmModule.forFeature([BlockedTimes])],
 
  controllers: [BlockedTimesController],
  providers: [BlockedTimesService],
