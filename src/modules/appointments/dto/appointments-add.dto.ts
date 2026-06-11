2e0aab67be4677af6ccd1c79926462b7062425d8
hosseinGith <hosseinderees7@gmail.com>
2026-06-11 23:48:47 +0330

    convert to dynamic dto

--- src/modules/appointments/dto/appointments-add.dto.ts
+++ src/modules/appointments/dto/appointments-add.dto.ts
@@ -1,24 +1,7 @@
-import { IsBoolean, IsDate, IsEnum, IsString } from 'class-validator';
-import {
- AppointmentStatusEnum,
- VisitTypeAppointmentsEnum,
-} from 'src/modules/appointments/entities/appointments.entity';
+import { OmitType } from '@nestjs/swagger';
+import { Appointments } from 'src/modules/appointments/entities/appointments.entity';
 
-export default class AppointmentsDtoAdd {
- @IsString()
- patientId: string;
- @IsString()
- doctorId: string;
- @IsDate()
- date: Date;
- @IsEnum(AppointmentStatusEnum)
- status: AppointmentStatusEnum;
- @IsEnum(VisitTypeAppointmentsEnum)
- visit_type: VisitTypeAppointmentsEnum;
- @IsString()
- symptoms: string;
- @IsBoolean()
- reminderSent: boolean;
- @IsString()
- hourId: string;
-}
+export default class AppointmentsDtoAdd extends OmitType(Appointments, [
+ 'created_at',
+ 'id',
+]) {}
